604f7bd0ddee3089a337914c0f71e418dc651e6d
KeithJF82 <keith.fraser@imperial.ac.uk>
2019-12-06 19:01:51 +0000

    Added first worked example

--- inst/create1.R
+++ inst/create1.R
@@ -1,11 +1,11 @@
 # This is an example of how to create a new dataset folder and populate it with equilibrium
 # starting data for a range of baseline malaria level values
 
-# Input location of 'vectorpower' folder, e.g. "C:/Users/Name/Documents/GitHub/"
-vectorpower_location = "C:/Users/Keith/Documents/Work/"  
+# Input location of parameter files
+input_location = "C:/Users/kjfras16/Documents/0 - Git repositories/vectorpower/inst/extdata/param_examples/"  
 
 # Input name of existing folder where new dataset folder is to be created
-output_location = "C:/Users/Keith/Documents/Work/vectorpower demo/"  
+output_location = "C:/Users/kjfras16/Documents/0 - R files/vectorpower testing/"  
 
 # Input name of dataset folder to be created in output_location
 dataset_location = "Demo Folder 1/"
@@ -20,9 +20,9 @@ nyears=1
 library(devtools)
 setwd(vectorpower_location)
 load_all("vectorpower")
-param_file=paste(vectorpower_location,"vectorpower/inst/extdata/param_examples/model_parameters_Mali.txt",sep="")
-age_file=paste(vectorpower_location,"vectorpower/inst/extdata/param_examples/age_data_145.txt",sep="")
-het_file=paste(vectorpower_location,"vectorpower/inst/extdata/param_examples/het_data_9.txt",sep="")
+param_file=paste(vectorpower_location,"model_parameters_Mali.txt",sep="")
+age_file=paste(vectorpower_location,"age_data_145.txt",sep="")
+het_file=paste(vectorpower_location,"het_data_9.txt",sep="")
 dataset_folder=paste(output_location,dataset_location,sep="")
 
 data1 <- dataset_create(dataset_folder=dataset_folder,param_file=param_file,age_file=age_file,
